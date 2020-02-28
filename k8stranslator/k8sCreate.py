6da31470f4d6383470587f5de79debe34533d661
princessfruitt <borisova.alex9@gmail.com>
2020-02-28 16:17:08 +0300

    (task:7102) add definition meta for deployment and service. Correct tosca file. Correct yaml parsing.

--- k8stranslator/k8sCreate.py
+++ k8stranslator/k8sCreate.py
@@ -9,6 +9,7 @@ TYPE = 'type'
 
 class KubernetesObj(object):
     def __init__(self, kind=None, api_version=None, properties=None):
+        self.properties = dict()
         self.properties = properties
         self.api_version = api_version
         self.kind = kind
@@ -34,16 +35,18 @@ def translate(template_file):
     node_templates = k8s_template.topology_template.nodetemplates
     k8s_list = []
     for template in node_templates:
-        props_list = [prop.value for prop in template.get_properties_objects() if
+        props_dict = dict()
+        [props_dict.update({prop.name : prop.value}) for prop in template.get_properties_objects() if
                       prop.name != API_VERSION and prop.name != API_GROUP]
         k8s_list.append(
             KubernetesObj(template.entity_tpl.get(TYPE).split('.'),
                           template.get_property_value(API_GROUP) + '/' + template.get_property_value(API_VERSION),
-                          props_list))
+                          props_dict))
     for k8s_obj in k8s_list:
-        complete_yaml = yaml.dump([dict(
+        k8s_yaml = dict(
             apiVersion=k8s_obj.get_version(),
-            kind=k8s_obj.get_kind()[2],
-            spec=k8s_obj.get_props()
-        )])
+            kind=k8s_obj.get_kind()[2])
+        for name, value in k8s_obj.get_props().items():
+            k8s_yaml.update({name:value})
+        complete_yaml = yaml.dump(k8s_yaml)
         print(complete_yaml)
